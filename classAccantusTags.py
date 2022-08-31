e1297b676e2eb175aeb54ac20389277ab7b6f9cd
aleksandra.stempin <stempin.aleksandra@gmail.com>
2022-08-31 11:33:12 +0200

    Accantus tags class added

---
+++ classAccantusTags.py
@@ -0,0 +1,57 @@
+import classMusicTags
+import os
+class MusicTagsForAccantus():
+    """sets tags (album and artist name and sets cover image) for Accantus songs"""
+    global _trackNoFilePath
+    _trackNoFilePath = r"AccantusTrackNo.txt"
+
+    def _ReadNumber(self):
+        """reads track number from file"""
+        trackNoInt = 1
+        if os.path.isfile(_trackNoFilePath):
+            trackNoFile = open(_trackNoFilePath, "r")
+            trackNo = trackNoFile.read()
+            trackNoFile.close()
+            try:
+                trackNoInt = int(trackNo)
+            except Exception as e:
+                print("ReadNumber\n{}".format(e))
+            finally:
+                return trackNoInt
+        else:
+            print("File {} doesn't exists".format(_trackNoFilePath))
+            return 1
+
+    def _WriteNumber(self, intNo):
+        """writes track number to file"""
+        try:
+            no = int(intNo)
+            noStr = str(no)
+            if os.path.isfile(_trackNoFilePath):
+                trackNoFile = open(_trackNoFilePath, "w")
+                trackNoFile.write(noStr)
+                trackNoFile.close()
+            else:
+                print("File {} doesn't exists".format(_trackNoFilePath))
+        except Exception as e:
+            print("WriteNumber\n{}".format(e))
+
+
+    def AccantusMusicTags(self, musicDirectory):
+        """sets tags to mp3 files in directory"""
+        coverPath = r"images/accantusLogo.jpg"
+        album = "Accantus"
+        artist = "Studio Accantus"
+        trackNo = MusicTagsForAccantus._ReadNumber(self)
+        mt = classMusicTags.MusicTags()
+        tn = mt.SetMusicTags(musicDirectory=musicDirectory
+                        ,albumName=album
+                        ,artistName=artist
+                        ,initTrackNumber=trackNo
+                        ,coverImg=coverPath)
+        MusicTagsForAccantus._WriteNumber(self,tn)
+
+
+
+
+
--- runMusicTags.py
+++ runMusicTags.py
@@ -1,28 +1,28 @@
 import classMusicTags
-
+import classAccantusTags
 # music directory path
-musicDir = r"C:\Users\Olenka\Downloads\new4"
+# musicDir = r"C:\Users\Olenka\Downloads\new4"
 musicDir = r"C:\Users\aleksandra.stempin\OneDrive - Accenture\!Sync\Downloads\music"
 
 # cower image path
 # coverImg = r"C:\Users\Olenka\Downloads\dog.jpg"
 # coverImg = r"C:\Users\Olenka\Downloads\accantusLogo.jpg"
-coverImg = r"C:\Users\aleksandra.stempin\OneDrive - Accenture\!Sync\Downloads\music\cover.jpg"
+# coverImg = r"C:\Users\aleksandra.stempin\OneDrive - Accenture\!Sync\Downloads\music\cover.jpg"
 # album name
 album = "pieselowo"
 # artist name
-artist = "piesely i kotely"
+artist = "piesely"
 
 startTrackNo = 1
 mt = classMusicTags.MusicTags()
 
-mt.SetMusicTags(
-    musicDirectory=musicDir
-    , albumName=album
-    , artistName=artist
-    , initTrackNumber=startTrackNo
-    , coverImg=coverImg
-)
+# mt.SetMusicTags(
+#     musicDirectory=musicDir
+#     , albumName=album
+#     , artistName=artist
+#     , initTrackNumber=startTrackNo
+#     , coverImg=coverImg
+# )
 # mt.SetMusicTagsCoverInTheDir(
 #     musicDirectory=musicDir
 #     # , albumName=album
@@ -31,3 +31,5 @@ mt.SetMusicTags(
 # )
 
 # mt.SetMusicTagsTagsInFileCoverInTheDir(musicDirectory=musicDir)
+mta = classAccantusTags.MusicTagsForAccantus()
+mta.AccantusMusicTags(musicDir)
